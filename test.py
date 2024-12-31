aa3defa08e57b5cb9d8f038be1fed521605c6b5c
Maciej601 <115342004+Maciej601@users.noreply.github.com>
2024-12-31 22:23:31 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("test python")
+from time import sleep
+
+# Program that asks for a name and greets the user
+
+number = int(input("Enter a number: "))
+for i in range(1, 51):
+    print(f"{number} * {i} = {number * i}")
+    sleep(0.5)
