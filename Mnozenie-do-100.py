cc6b177f90b7143dd5bfd018197be86e7917beca
Maciej601 <115342004+Maciej601@users.noreply.github.com>
2025-01-01 01:00:53 +0000

    modified:   Mnozenie-do-100.py

--- Mnozenie-do-100.py
+++ Mnozenie-do-100.py
@@ -3,6 +3,8 @@ from time import sleep
 # Program that asks for a name and greets the user
 
 number = int(input("Podaj Liczbę: "))
-for i in range(1, 101):
+print("Teraz pomnożę tą liczbę od 1 do 30")
+sleep(2)
+for i in range(1, 31):
     print(f"{number} * {i} = {number * i}")
     sleep(0.5)
